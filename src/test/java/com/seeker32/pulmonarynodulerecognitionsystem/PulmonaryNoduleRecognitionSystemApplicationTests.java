5414a688e45224ff89809dc6cded712a782a1912
Seeker32 <gz_159762@163.com>
2025-03-17 15:02:18 +0800

    initial commit

---
+++ src/test/java/com/seeker32/pulmonarynodulerecognitionsystem/PulmonaryNoduleRecognitionSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seeker32.pulmonarynodulerecognitionsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PulmonaryNoduleRecognitionSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
